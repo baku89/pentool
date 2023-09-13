654f077a13a04692958f2947dd1a208e6a968517
Baku Hashimoto <mail@baku89.com>
2023-09-14 00:00:17 +0900

    Add support for PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
 import monacoEditorPlugin, {
 	type IMonacoEditorOpts,
@@ -12,6 +13,31 @@ export default defineConfig({
 		vue(),
 		monacoEditorPluginDefault({
 			languageWorkers: ['editorWorkerService', 'typescript'],
+		}),
+		VitePWA({
+			registerType: 'autoUpdate',
+			devOptions: {
+				enabled: true,
+			},
+			injectRegister: 'auto',
+			includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+			manifest: {
+				name: 'Paper.js Editor',
+				short_name: 'Paper.js',
+				theme_color: '#ffffff',
+				icons: [
+					{
+						src: 'icon-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: 'icon-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+					},
+				],
+			},
 		}),
 	],
 })
