0fe0f28e52532857d41b6d1cfdea194159b6479f
Baku Hashimoto <mail@baku89.com>
2023-09-14 21:20:50 +0900

    Clean main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,3 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
-
-// // enable copy and paste features
-// useCopyPasteFeatures(editor)
-
-// // direct manipulation features
-// const { updateOverlays } = useDirectManipulation(editor)
