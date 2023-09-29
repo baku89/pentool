c0bb93a6099bbd84c58c3545ba9a4fc07b095d31
Baku Hashimoto <mail@baku89.com>
2023-09-30 03:30:10 +0900

    Rewrite InputRotery with Vue3's setup

--- src/tweeq/InputRotery/InputRotery.vue
+++ src/tweeq/InputRotery/InputRotery.vue
@@ -1,14 +1,29 @@
-<script lang="ts">
+<script lang="ts" setup>
 import {some} from 'fp-ts/lib/Option'
 import {checkIntersection} from 'line-intersect'
 import {Vec2, vec2} from 'linearly'
 import _ from 'lodash'
-import {computed, defineComponent, Ref, ref} from 'vue'
+import {computed, Ref, ref} from 'vue'
 
 import SvgIcon from '../SvgIcon.vue'
 import useDrag from '../useDragV1'
 import useModelLocalDisplay from '../useModelLocalDisplay'
 import {unsignedMod} from '../util'
+
+interface Props {
+	modelValue: number
+	updateOnBlur?: boolean
+}
+
+const props = withDefaults(defineProps<Props>(), {updateOnBlur: true})
+
+const emit = defineEmits<{
+	'update:modelValue': [number]
+}>()
+
+defineOptions({
+	inheritAttrs: false,
+})
 
 function signedAngleBetween(target: number, source: number) {
 	const ret = target - source
@@ -22,220 +37,184 @@ function addDirectionVector(from: Vec2, rads: number, radius: number): Vec2 {
 const PI = Math.PI
 const PI_2 = Math.PI * 2
 
-export default defineComponent({
-	name: 'InputRotery',
-	components: {
-		SvgIcon,
+const {local, display} = useModelLocalDisplay({
+	props,
+	emit,
+	print(rad) {
+		const deg = (rad / PI) * 180
+		return deg.toFixed(1) + '°'
 	},
-	inheritAttrs: false,
-	props: {
-		modelValue: {
-			type: Number,
-			required: true,
-		},
-		updateOnBlur: {
-			type: Boolean,
-			default: true,
-		},
+	parse: v => some(parseFloat(v)),
+})
+
+const el: Ref<null | HTMLElement> = ref(null)
+
+const tweakMode = ref<'relative' | 'absolute'>('relative')
+
+let alreadyEmitted = false
+const tweakOrigin = ref(local.value)
+let tweakRawLocal = local.value
+
+const {
+	isDragging: tweaking,
+	origin,
+	pos,
+} = useDrag(el, {
+	disableClick: true,
+	// lockPointer: true,
+	onDragStart({pos, origin}) {
+		if (tweakMode.value === 'absolute') {
+			const p = vec2.sub(pos, origin)
+			const angle = Math.atan2(p[1], p[0])
+			const delta = signedAngleBetween(angle, local.value)
+			tweakRawLocal = local.value + delta
+
+			tweakOrigin.value = tweakRawLocal
+			local.set(tweakRawLocal)
+			alreadyEmitted = true
+		} else {
+			// Relative
+			tweakRawLocal = local.value
+			tweakOrigin.value = local.value
+		}
 	},
-	emits: ['update:modelValue'],
-	setup(props, {emit}) {
-		const {local, display} = useModelLocalDisplay({
-			props,
-			emit,
-			print(rad) {
-				const deg = (rad / PI) * 180
-				return deg.toFixed(1) + '°'
-			},
-			parse: v => some(parseFloat(v)),
-		})
+	onDrag({pos, prevPos, origin}) {
+		if (alreadyEmitted) {
+			alreadyEmitted = false
+			return
+		}
 
-		const el: Ref<null | HTMLElement> = ref(null)
+		const p = vec2.sub(pos, origin)
+		const pp = vec2.sub(prevPos, origin)
 
-		const tweakMode = ref<'relative' | 'absolute'>('relative')
+		const prevAngle = Math.atan2(pp[1], pp[0])
+		const alignedPos = vec2.rotate(p, -prevAngle)
+		const delta = Math.atan2(alignedPos[1], alignedPos[0])
+		tweakRawLocal += delta
+		local.set(tweakRawLocal)
+	},
+	onDragEnd() {
+		tweakMode.value = 'relative'
+		local.conform()
+	},
+})
 
-		let alreadyEmitted = false
-		const tweakOrigin = ref(local.value)
-		let tweakRawLocal = local.value
+const rem = ref(16) //useRem()
 
-		const {
-			isDragging: tweaking,
-			origin,
-			pos,
-		} = useDrag(el, {
-			disableClick: true,
-			// lockPointer: true,
-			onDragStart({pos, origin}) {
-				if (tweakMode.value === 'absolute') {
-					const p = vec2.sub(pos, origin)
-					const angle = Math.atan2(p[1], p[0])
-					const delta = signedAngleBetween(angle, local.value)
-					tweakRawLocal = local.value + delta
+const overlayArrowAngle = computed(() => {
+	const p = vec2.sub(pos.value, origin.value)
+	return Math.atan2(p[1], p[0]) + Math.PI / 2
+})
 
-					tweakOrigin.value = tweakRawLocal
-					local.set(tweakRawLocal)
-					alreadyEmitted = true
-				} else {
-					// Relative
-					tweakRawLocal = local.value
-					tweakOrigin.value = local.value
-				}
-			},
-			onDrag({pos, prevPos, origin}) {
-				if (alreadyEmitted) {
-					alreadyEmitted = false
-					return
-				}
+const overlayArcPath = computed(() => {
+	if (!tweaking.value) return ''
 
-				const p = vec2.sub(pos, origin)
-				const pp = vec2.sub(prevPos, origin)
+	const baseRadius = rem.value * 8
+	const radiusStep = rem.value * 0.6
 
-				const prevAngle = Math.atan2(pp[1], pp[0])
-				const alignedPos = vec2.rotate(p, -prevAngle)
-				const delta = Math.atan2(alignedPos[1], alignedPos[0])
-				tweakRawLocal += delta
-				local.set(tweakRawLocal)
-			},
-			onDragEnd() {
-				tweakMode.value = 'relative'
-				local.conform()
-			},
-		})
+	const start = tweakOrigin.value
+	const end = local.value
 
-		const rem = ref(16) //useRem()
+	const tweakingPositive = end - start > 0
 
-		const overlayArrowAngle = computed(() => {
-			const p = vec2.sub(pos.value, origin.value)
-			return Math.atan2(p[1], p[0]) + Math.PI / 2
-		})
+	const turns =
+		Math.floor(Math.abs(end - start) / PI_2) * Math.sign(end - start)
 
-		const overlayArcPath = computed(() => {
-			if (!tweaking.value) return ''
+	const c = origin.value
 
-			const baseRadius = rem.value * 8
-			const radiusStep = rem.value * 0.6
+	// Create arc
+	const arcRadius = baseRadius + turns * radiusStep
 
-			const start = tweakOrigin.value
-			const end = local.value
+	let offsetInTurn = unsignedMod(signedAngleBetween(end, start), PI_2)
+	offsetInTurn = tweakingPositive ? offsetInTurn : offsetInTurn - PI_2
 
-			const tweakingPositive = end - start > 0
+	const startInTurn = unsignedMod(start, PI_2)
+	const endInTurn = startInTurn + offsetInTurn
 
-			const turns =
-				Math.floor(Math.abs(end - start) / PI_2) * Math.sign(end - start)
+	const from = addDirectionVector(c, startInTurn, arcRadius)
+	const to = addDirectionVector(c, endInTurn, arcRadius)
 
-			const c = origin.value
+	const angleBetween = Math.abs(startInTurn - endInTurn)
 
-			// Create arc
-			const arcRadius = baseRadius + turns * radiusStep
+	const largeArcFlag = angleBetween > PI ? 1 : 0
+	const sweepFlag = tweakingPositive ? 1 : 0
 
-			let offsetInTurn = unsignedMod(signedAngleBetween(end, start), PI_2)
-			offsetInTurn = tweakingPositive ? offsetInTurn : offsetInTurn - PI_2
-
-			const startInTurn = unsignedMod(start, PI_2)
-			const endInTurn = startInTurn + offsetInTurn
-
-			const from = addDirectionVector(c, startInTurn, arcRadius)
-			const to = addDirectionVector(c, endInTurn, arcRadius)
-
-			const angleBetween = Math.abs(startInTurn - endInTurn)
-
-			const largeArcFlag = angleBetween > PI ? 1 : 0
-			const sweepFlag = tweakingPositive ? 1 : 0
-
-			const arc = `
+	const arc = `
 					M ${from.join(' ')}
 					A ${arcRadius} ${arcRadius}
 						0 ${largeArcFlag} ${sweepFlag}
 						${to.join(' ')} `
 
-			// Create revolutions
-			let circles = ''
-			for (let i = 0, step = Math.sign(turns); i !== turns; i += step) {
-				const radius = baseRadius + i * radiusStep
-				if (radius < 0) {
-					continue
-				}
-				const right = `${c[0] + radius} ${c[1]}`
-				const left = `${c[0] - radius} ${c[1]}`
-				circles += `M ${right}
+	// Create revolutions
+	let circles = ''
+	for (let i = 0, step = Math.sign(turns); i !== turns; i += step) {
+		const radius = baseRadius + i * radiusStep
+		if (radius < 0) {
+			continue
+		}
+		const right = `${c[0] + radius} ${c[1]}`
+		const left = `${c[0] - radius} ${c[1]}`
+		circles += `M ${right}
 										A ${radius} ${radius} 0 1 0 ${left}
 										A ${radius} ${radius} 0 1 0 ${right}`
-			}
+	}
 
-			return arc + circles
-		})
+	return arc + circles
+})
 
-		function clampPos(p: Vec2): Vec2 {
-			const [ox, oy] = origin.value
-			const [x, y] = p
-			const margin = 40
-			const left = margin,
-				top = margin,
-				right = window.innerWidth - margin,
-				bottom = window.innerHeight - margin
+function clampPos(p: Vec2): Vec2 {
+	const [ox, oy] = origin.value
+	const [x, y] = p
+	const margin = 40
+	const left = margin,
+		top = margin,
+		right = window.innerWidth - margin,
+		bottom = window.innerHeight - margin
 
-			let ret: ReturnType<typeof checkIntersection>
+	let ret: ReturnType<typeof checkIntersection>
 
-			const check = _.partial(checkIntersection, x, y, ox, oy)
+	const check = _.partial(checkIntersection, x, y, ox, oy)
 
-			if ((ret = check(left, top, right, top)).type === 'intersecting') {
-				return [ret.point.x, ret.point.y]
-			}
+	if ((ret = check(left, top, right, top)).type === 'intersecting') {
+		return [ret.point.x, ret.point.y]
+	}
 
-			if ((ret = check(right, top, right, bottom)).type === 'intersecting') {
-				return [ret.point.x, ret.point.y]
-			}
+	if ((ret = check(right, top, right, bottom)).type === 'intersecting') {
+		return [ret.point.x, ret.point.y]
+	}
 
-			if ((ret = check(right, bottom, left, bottom)).type === 'intersecting') {
-				return [ret.point.x, ret.point.y]
-			}
+	if ((ret = check(right, bottom, left, bottom)).type === 'intersecting') {
+		return [ret.point.x, ret.point.y]
+	}
 
-			if ((ret = check(left, bottom, left, top)).type === 'intersecting') {
-				return [ret.point.x, ret.point.y]
-			}
+	if ((ret = check(left, bottom, left, top)).type === 'intersecting') {
+		return [ret.point.x, ret.point.y]
+	}
 
-			return [x, y]
-		}
+	return [x, y]
+}
 
-		const overlayLineTo = computed(() => {
-			const dist = vec2.distance(origin.value, pos.value)
-			const dir: Vec2 = [Math.cos(local.value), Math.sin(local.value)]
+const overlayLineTo = computed(() => {
+	const dist = vec2.distance(origin.value, pos.value)
+	const dir: Vec2 = [Math.cos(local.value), Math.sin(local.value)]
 
-			const p = vec2.scaleAndAdd(origin.value, dir, dist)
+	const p = vec2.scaleAndAdd(origin.value, dir, dist)
 
-			return clampPos(p)
-		})
+	return clampPos(p)
+})
 
-		const overlayLabelPos = computed(() => {
-			return clampPos(pos.value)
-		})
+const overlayLabelPos = computed(() => {
+	return clampPos(pos.value)
+})
 
-		const overlayLineFrom = computed(() => {
-			const o = origin.value
-			const t = overlayLineTo.value
-			const radius = 10
+const overlayLineFrom = computed(() => {
+	const o = origin.value
+	const t = overlayLineTo.value
+	const radius = 10
 
-			const dir = vec2.normalize(vec2.sub(t, o))
+	const dir = vec2.normalize(vec2.sub(t, o))
 
-			return vec2.scaleAndAdd(o, dir, radius)
-		})
-
-		return {
-			el,
-			local,
-			tweaking,
-			tweakMode,
-			tweakOrigin,
-
-			// overlay
-			display,
-			overlayLineTo,
-			overlayLineFrom,
-			overlayArcPath,
-			overlayArrowAngle,
-			overlayLabelPos,
-		}
-	},
+	return vec2.scaleAndAdd(o, dir, radius)
 })
 </script>
 
